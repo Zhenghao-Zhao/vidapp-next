f8ce927bec99a9b34927ccd45ba940f364c56104
Zhenghao-Zhao <grekinaus@gmail.com>
2024-01-01 01:53:49 +0800

    added feature that allows users to move scaled image

--- src/app/_components/images/ResizeableImage.tsx
+++ src/app/_components/images/ResizeableImage.tsx
@@ -1,19 +1,72 @@
-import React, { useState } from "react";
-import ZoomBar from "./ZoomBar";
+import React, { useEffect, useRef, useState } from "react";
+import ResizeScrollbar from "./ResizeScrollbar";
 
 export default function ResizeableImage({
   isCurrent,
   children,
 }: {
-  isCurrent: boolean,
+  isCurrent: boolean;
   children: React.ReactNode;
 }) {
   const [scale, setScale] = useState(1);
+  const [margin, setMargin] = useState({
+    bottom: 0,
+    right: 0,
+  });
+
+  const mouseDownRef = useRef({x: 0, y: 0});
+  const prevRef = useRef({x: 0, y: 0}); 
+  const translateRef = useRef({x: 0, y: 0})
+  const [refresh, setRefresh] = useState({});
+
+  useEffect(() => {
+    setMargin({
+      bottom: (scale - 1) * 400,
+      right: (scale - 1) * 400,
+    });
+  }, [scale]);
+
+  console.log(margin)
+
+  const handleMouseDown = (e: React.MouseEvent) => {
+    // console.log('hi')
+    e.preventDefault();
+    document.addEventListener("mouseup", handleMouseUp);
+    document.addEventListener("mousemove", handleMouseMove);
+    mouseDownRef.current = {x: e.clientX, y: e.clientY};
+    prevRef.current = {...translateRef.current};
+  };
+
+  const handleMouseMove = (e: MouseEvent) => {
+    // translateRef.current.x = Math.min(Math.max(e.clientX - mouseDownRef.current.x+ prevRef.current.x, -margin.left), margin.right);
+    // translateRef.current.y = Math.min(Math.max(e.clientY - mouseDownRef.current.y+ prevRef.current.y, -margin.top), margin.bottom);
+    translateRef.current.x = Math.min(Math.max(e.clientX - mouseDownRef.current.x + prevRef.current.x, -margin.right), margin.right);
+    translateRef.current.y = Math.min(Math.max(e.clientY - mouseDownRef.current.y + prevRef.current.y, -margin.bottom), margin.bottom);
+    setRefresh({});
+  };
+
+  const handleMouseUp = (e: MouseEvent) => {
+    document.removeEventListener("mousemove", handleMouseMove);
+    document.removeEventListener("mouseup", handleMouseUp);
+  };
 
   return (
-    <div className={`relative h-full w-full flex items-center justify-center ${isCurrent && 'z-10'}`}>
-      <div className="h-full w-full" style={{ transform: `scale(${scale})` }}>{children}</div>
-      <ZoomBar setScale={setScale} />
+    <div
+      className={`relative h-full w-full flex items-center justify-center ${
+        isCurrent && "z-10"
+      }`}
+    >
+      <div
+        onMouseDown={handleMouseDown}
+        className="h-full w-full"
+        style={{
+          transform: `translate3d(${translateRef.current.x}px, 
+            ${translateRef.current.y}px, 0px) scale(${scale})`,
+        }}
+      >
+        {children}
+      </div>
+      <ResizeScrollbar setScale={setScale} />
     </div>
   );
 }
