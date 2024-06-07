085d97b0d10f6d9c7b0d0b817395b35e10712555
Zhenghao-Zhao <grekinaus@gmail.com>
2024-06-07 15:32:16 +0800

    using grid for responsive layout in page header

--- src/app/(pages)/[username]/_content/header/index.tsx
+++ src/app/(pages)/[username]/_content/header/index.tsx
@@ -4,10 +4,9 @@ import { ModalContent, ModalTrigger } from "@/app/_components/ui/modal";
 import Modal from "@/app/_libs/contexts/providers/ModalContextProivder";
 import { Profile } from "@/app/_libs/types";
 import FollowButton from "../../_components/FollowButton";
+import FriendList from "../../_components/FriendsList";
 import ProfileChanger from "../../_components/ProfileChanger";
 import ProfileImage from "../../_components/ProfileImage";
-import FriendList from "../../_components/FriendsList";
-import { twMerge } from "tailwind-merge";
 
 export default function Header({
   profile,
@@ -20,12 +19,17 @@ export default function Header({
     <header className="grid g-header-template border-b p-4 mb-4 justify-start items-center">
       <div className="area-profile">
         {isOwner ? (
-          <ProfileChanger twSize="size-[120px] sm:size-profile-image-size" />
+          <ProfileChanger twSize="size-[110px] sm:size-profile-image-size" />
         ) : (
-          <ProfileImage imageURL={profile.imageURL} twSize="size-[120px] sm:size-profile-image-size" />
+          <ProfileImage
+            imageURL={profile.imageURL}
+            twSize="size-[110px] sm:size-profile-image-size"
+          />
         )}
       </div>
-      <p className="text-2xl font-bold area-name justify-self-start">{profile.name}</p>
+      <p className="text-2xl font-bold area-name justify-self-start">
+        {profile.name}
+      </p>
       {!isOwner && (
         <FollowButton
           has_followed={profile.has_followed}
@@ -33,50 +37,36 @@ export default function Header({
           className="area-follow"
         />
       )}
-      <Stats profile={profile} isOwner={isOwner} className="area-stats" />
+      <div className="flex gap-[20px] items-center justify-between area-stats">
+        <p>
+          <span className="mr-2 font-bold">
+            {profile.post_count.toLocaleString()}
+          </span>
+          posts
+        </p>
+        <Modal>
+          <ModalTrigger>
+            <button>
+              <span className="mr-2 font-bold">{profile.follower_count}</span>
+              followers
+            </button>
+          </ModalTrigger>
+          <ModalContent>
+            <FriendList uid={profile.uid} friendship="followers" />
+          </ModalContent>
+        </Modal>
+        <Modal>
+          <ModalTrigger>
+            <button>
+              <span className="mr-2 font-bold">{profile.followee_count}</span>
+              following
+            </button>
+          </ModalTrigger>
+          <ModalContent>
+            <FriendList uid={profile.uid} friendship="followees" />
+          </ModalContent>
+        </Modal>
+      </div>
     </header>
-  );
-}
-
-function Stats({
-  profile,
-  isOwner,
-  className,
-}: {
-  profile: Profile;
-  isOwner: boolean;
-  className: string;
-}) {
-  return (
-    <div className={twMerge("flex gap-[20px] items-center justify-between", className)}>
-      <p>
-        <span className="mr-2 font-bold">
-          {profile.post_count.toLocaleString()}
-        </span>
-        posts
-      </p>
-      <Modal>
-        <ModalTrigger>
-          <button>
-            <span className="mr-2 font-bold">{profile.follower_count}</span>
-            followers
-          </button>
-        </ModalTrigger>
-        <ModalContent>
-          <FriendList uid={profile.uid} friendship="followers" />
-        </ModalContent>
-      </Modal>
-      <Modal>
-        <ModalTrigger>
-          <button>
-            <span className="mr-2 font-bold">{profile.followee_count}</span>
-            following
-          </button>
-        </ModalTrigger>
-        <ModalContent>
-          <FriendList uid={profile.uid} friendship="followees" />
-        </ModalContent>
-      </Modal>
-    </div>
   );
 }
